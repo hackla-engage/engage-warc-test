93fb1edfa926557c18bccaf056f022acc84c819a
hungrymonkey <tedchang2010@gmail.com>
2019-03-04 19:47:13 -0800

    kinda broken warc module

--- scripts/print_all_urls.py
+++ scripts/print_all_urls.py
@@ -4,8 +4,11 @@ import warc
 import os
 
 
-COLLECTIONS_DIR = "../collections/smgov/archive"
+COLLECTIONS_DIR = '../collections/smgov/archive/'
 
 if __name__ == "__main__":
 	warc_files = os.listdir(COLLECTIONS_DIR)
-	print(warc_files)
+	for f_name in warc_files:
+		f = warc.WARCFile( COLLECTIONS_DIR + str(f_name), 'r')
+		for record in f:
+			print( record )

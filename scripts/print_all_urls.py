5e1be6e4441efc764a5cb39ecd03dcc7eaaf4227
hungrymonkey <tedchang2010@gmail.com>
2019-03-11 18:40:16 -0700

    warc module is not broken

--- scripts/print_all_urls.py
+++ scripts/print_all_urls.py
@@ -9,6 +9,6 @@ COLLECTIONS_DIR = '../collections/smgov/archive/'
 if __name__ == "__main__":
 	warc_files = os.listdir(COLLECTIONS_DIR)
 	for f_name in warc_files:
-		f = warc.WARCFile( COLLECTIONS_DIR + str(f_name), 'r')
+		f = warc.open( COLLECTIONS_DIR + str(f_name))
 		for record in f:
-			print( record )
+			print( record['WARC-Target-URI'] )

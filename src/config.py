7bfa3b3c2f7d2300eacafbb8d7287c4ce7fe1fd6
hungrymonkey <tedchang2010@gmail.com>
2019-03-18 19:04:06 -0700

    fix get waybackurl

--- src/config.py
+++ src/config.py
@@ -1,17 +1,19 @@
 import os
 import subprocess
+import time
 
 PROJECT_ROOT = os.path.realpath(__file__ + "/../../")
 
 COLLECTIONS_PATH = PROJECT_ROOT + '/collections/smgov/archive/'
 
 COLLECTION_NAME = 'smgov'
-SERVER = 'localhost:8080'
+SERVER = 'http://localhost:8080'
 
 def initialize():
 	global _wayback_process
 	os.chdir(PROJECT_ROOT) #chroot to project root dir
 	_wayback_process = subprocess.Popen(["wayback"]) #start wayback machine
+	time.sleep(10)
 
 def terminate():
 	_wayback_process.kill()
--- src/test_website.py
+++ src/test_website.py
@@ -1,5 +1,6 @@
 import config
-import urllib
+import urllib.request
+import sys
 
 from generate_urls import ArchiveUrl
 from generate_urls import generateURL
@@ -7,5 +8,10 @@ from generate_urls import generateURL
 
 if __name__ == "__main__":
 	config.initialize()
-	
+	for archive_url in generateURL():
+		try:
+			with urllib.request.urlopen(archive_url.get_wayback_url()) as response:
+				print(response.read())
+		except:
+			print("Error url fetch failed: " + archive_url.get_wayback_url(), file=sys.stderr)
 	config.terminate()

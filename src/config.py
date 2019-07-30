85477ec31ed6df4a5d574886f16bc446c5b9aac7
hungrymonkey <tedchang2010@gmail.com>
2019-07-29 19:29:14 -0700

    added more paths to config

--- src/config.py
+++ src/config.py
@@ -5,6 +5,8 @@ import time
 PROJECT_ROOT = os.path.realpath(__file__ + "/../../")
 
 COLLECTIONS_PATH = PROJECT_ROOT + '/collections/smgov/archive/'
+TEST_DIRECTORY_PATH = PROJECT_ROOT + '/tests/'
+AGENDA_ITEM_PATH = TEST_DIRECTORY_PATH + 'agenda_item/'
 
 COLLECTION_NAME = 'smgov'
 SERVER = 'http://localhost:8080'
